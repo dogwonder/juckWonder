b53563e04ab0448a07f295b767f0375633645996
Rich Holman <dogwonder@gmail.com>
2019-10-23 16:08:26 +0100

    Offline sw.js

--- src/sw.njk.js
+++ src/sw.njk.js
@@ -1,85 +1,48 @@
 const CACHE = "dgCache-{{ pkgVersion() }}";
-const precacheFiles = [
-    '/',
-    '/css/main.css',
-    '/scripts/app.js',
-    '/favicon.ico',
-    '/images/fav/android-chrome-192x192.png'
-];
+const offlineFallbackPage = "offline.html";
 
+// Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener("install", function (event) {
   console.log("Install Event processing");
 
-  console.log("Skip waiting on install");
-  self.skipWaiting();
-
   event.waitUntil(
     caches.open(CACHE).then(function (cache) {
-      console.log("Caching pages during install");
-      return cache.addAll(precacheFiles);
+      console.log("Cached offline page during install");
+      return cache.add(offlineFallbackPage);
     })
   );
 });
 
-// Allow sw to control of current page
-self.addEventListener("activate", function (event) {
-  console.log("Claiming clients for current page");
-  event.waitUntil(self.clients.claim());
-});
-
-// If any fetch fails, it will look for the request in the cache and serve it from there first
-self.addEventListener("fetch", function (event) { 
+// If any fetch fails, it will show the offline page.
+self.addEventListener("fetch", function (event) {
   if (event.request.method !== "GET") return;
 
   event.respondWith(
-    fromCache(event.request).then(
-      function (response) {
-        // The response was found in the cache so we responde with it and update the entry
-
-        // This is where we call the server to get the newest version of the
-        // file to use the next time we show view
-        event.waitUntil(
-          fetch(event.request).then(function (response) {
-            return updateCache(event.request, response);
-          })
-        );
-
-        return response;
-      },
-      function () {
-        // The response was not found in the cache so we look for it on the server
-        return fetch(event.request)
-          .then(function (response) {
-            // If request was success, add or update it in the cache
-            event.waitUntil(updateCache(event.request, response.clone()));
-
-            return response;
-          })
-          .catch(function (error) {
-            console.log("Network request failed and no cache." + error);
-          });
+    fetch(event.request).catch(function (error) {
+      // The following validates that the request was for a navigation to a new document
+      if (
+        event.request.destination !== "document" ||
+        event.request.mode !== "navigate"
+      ) {
+        return;
       }
-    )
+
+      console.error("Network request Failed. Serving offline page " + error);
+      return caches.open(CACHE).then(function (cache) {
+        return cache.match(offlineFallbackPage);
+      });
+    })
   );
 });
 
-function fromCache(request) {
-  // Check to see if you have it in the cache
-  // Return response
-  // If not in the cache, then return
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      if (!matching || matching.status === 404) {
-        return Promise.reject("no-match");
-      }
+// This is an event that can be fired from your page to tell the SW to update the offline page
+self.addEventListener("refreshOffline", function () {
+  const offlinePageRequest = new Request(offlineFallbackPage);
 
-      return matching;
+  return fetch(offlineFallbackPage).then(function (response) {
+    return caches.open(CACHE).then(function (cache) {
+      console.log("Offline page updated from refreshOffline event: " + response.url);
+      return cache.put(offlinePageRequest, response);
     });
   });
-}
-
-function updateCache(request, response) {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.put(request, response);
-  });
-}
+});
