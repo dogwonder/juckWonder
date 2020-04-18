937c471c7744507d21dbde2a33e7978e4c3a1b46
Rich Holman <dogwonder@gmail.com>
2020-04-18 15:57:37 +0100

    update copyright year

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "dgCache-1.216.0";
+const CACHE = "dgCache-1.217.0";
 
 // This is the service worker with the Cache-first network
 const precacheFiles = [
@@ -38,7 +38,7 @@ self.addEventListener("activate", function(event) {
         })
       );
     }).then(function() {
-      console.log('[ServiceWorker] Claiming clients for version - 1.216.0');
+      console.log('[ServiceWorker] Claiming clients for version - 1.217.0');
       return self.clients.claim();
     })  
   );
