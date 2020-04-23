c0dfe35feef332d69ecb5d501a0c7c0eca496d7c
Rich Holman <dogwonder@gmail.com>
2020-04-23 09:30:49 +0100

    remove brotli compression

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "dgCache-1.219.0";
+const CACHE = "dgCache-1.220.0";
 
 // This is the service worker with the Cache-first network
 const precacheFiles = [
@@ -38,7 +38,7 @@ self.addEventListener("activate", function(event) {
         })
       );
     }).then(function() {
-      console.log('[ServiceWorker] Claiming clients for version - 1.219.0');
+      console.log('[ServiceWorker] Claiming clients for version - 1.220.0');
       return self.clients.claim();
     })  
   );
